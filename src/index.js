13fc2d834b225ba3f24be52612f1b733c9d98a07
Ajay Chaudhary <ajaychaudhary7980@gmail.com>
2021-02-06 12:12:05 +0530

    all live-time folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const date = new Date();
+const currentTime = date.getHours();
+var weekday = date.toLocaleString("default", { weekday: "long" });
+
+let greeting;
+
+if(currentTime < 12){
+  greeting = "Good Morning"
+}
+else if(currentTime < 18){
+  greeting = "Good evening"
+}
+else{
+  greeting = "Good Night"
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+<h1> {greeting}</h1>
+<h2>Today is {weekday}</h2>
+  </div>
+  ,document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
